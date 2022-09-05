5f1496fbba7847ed4d1301aced2de48eb167d58f
alfons <alfonsspj@gmail.com>
2022-09-05 01:09:00 -0300

    Creando Provider

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,7 +1,38 @@
 
 const Formulario = () => {
   return (
-    <div>Formulario</div>
+    <div className="contenedor">
+        <form>
+            <div className="campo">
+                <label htmlFor="ciudad">Ciudad</label>
+                <input
+                    type="text"
+                    id="ciudad"
+                    name="ciudad"
+                />                
+            </div>
+            <div className="campo">
+                <label htmlFor="pais">Pais</label>
+                <select
+                    id="pais"
+                    name="pais"
+                >
+                    <option value="">Seleccione un País</option>
+                    <option value="US">Estados Unidos</option>
+                    <option value="MX">México</option>
+                    <option value="AR">Argentina</option>
+                    <option value="CO">Colombia</option>
+                    <option value="CR">España</option>
+                    <option value="PE">Perú</option>
+                </select>                
+            </div>
+
+            <input 
+                type="submit"
+                value='Consultar Clima'
+            />
+        </form>
+    </div>
   )
 }
 
