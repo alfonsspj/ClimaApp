727084d45d8ebbd75c0572e585a7025aa626bd71
alfons <alfonsspj@gmail.com>
2022-09-05 19:10:47 -0300

    Accediendo y mostrando informacion del clima

--- src/components/AppClima.jsx
+++ src/components/AppClima.jsx
@@ -1,10 +1,19 @@
 import Formulario from './Formulario'
+import Resultado from './Resultado'
+import useClima from '../hooks/useClima'
 
 const AppClima = () => {
+
+  // es un objeto que esta vacio
+  const { resultado } = useClima()
+
   return (
     <>
         <main className="dos-columnas">
             <Formulario />
+
+             {/* optional change - cuando tienes un objeto vacio y despues va a tener algo */}
+            {resultado?.name && <Resultado />}
         </main>
     </>
   )
